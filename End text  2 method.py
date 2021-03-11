243c4c981455218a187ddfa163442ce76a53967d
gargisingh160201 <65045803+gargisingh160201@users.noreply.github.com>
2021-03-12 00:29:03 +0530

    Delete End text  2 method.py

--- End text  2 method.py
+++
@@ -1,6 +0,0 @@
-recieved_string = " hello I am having issues in using hello. The views and opinions included in this email belong to their author and do not necessarily mirror the views and opinions of the company. Our employees are obliged not to make any defamatory clauses, infringe, or authorize infringement of any legal right. Therefore, the company will not take any liability for such statements included in emails. In case of any damages or other liabilities arising, employees are fully responsible for the content of their emails."
-string_to_be_removed = "The views and opinions included in this email belong to their author and do not necessarily mirror the views and opinions of the company. Our employees are obliged not to make any defamatory clauses, infringe, or authorize infringement of any legal right. Therefore, the company will not take any liability for such statements included in emails. In case of any damages or other liabilities arising, employees are fully responsible for the content of their emails"
-# removing substring from end using replace method to replace it with white space
-if recieved_string.endswith(string_to_be_removed ):
-      res = re.replace(string_to_be_removed , '')
-print(res).
